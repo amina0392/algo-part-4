c89ffed58904fc790a7990fa1b1fd846a849679a
Amina ALI <aliyam3@yahoo.fr>
2023-11-20 16:34:25 +0100

    Modification du code JavaScript eleve.js

--- js/eleve.js
+++ js/eleve.js
@@ -1,25 +1,37 @@
 class Eleve {
-    constructor (nom, prenom, classe,) {
+    constructor (nom, prenom, classe) {
      this.name = nom;
      this.firstname = prenom;
      this.cours = classe;
      this.notes = {maths: [], algorithmique: [], javascript: []};
+     
     } 
     
     monPrenom() {
       console.log(`Bonjour, mon prenom est ${this.firstname}`);
     } 
     maFormation() {
-        console.log(`Bonjour, je suis en formation ${this.cours}`);
+        console.log(`Marie est en formation ${this.cours}`);
     }
-  
+    marieNotes() {   
+       this.notes.maths.push(12);
+       console.log(`La note de maths de Marie est de ${this.notes.maths}`);
+    }
+    jonhNotes () {
+      this.notes.maths.push(14);
+      console.log(`La note de maths de John est de ${this.notes.maths}`);
+    }
 }
 let pierre = new Eleve("Dupont", "Pierre", "Kercode");
 let marie = new Eleve("Fontaine", "Marie", "CDA");
 let jonh = new Eleve("Hunter", "Jonh", "CDUI");
 
+
 pierre.monPrenom();
 marie.maFormation();
+marie.marieNotes();
+jonh.jonhNotes();
+
 
 
     
